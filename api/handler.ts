2e51267a9c9f74e54b08d6fbc553d876f2afca9e
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2025-06-01 11:37:03 -0300

    add express server

---
+++ api/handler.ts
@@ -0,0 +1,29 @@
+import { format } from 'url'
+import { ExtendedRequest } from "./types";
+
+
+export default async function handler(req: ExtendedRequest) { 
+    console.warn("URL: " + req.url)
+    if (!req.url) { return new Response(null, { status: 400 }) }
+    let { hostname: queryHostname } = req.query ?? {}
+    const { hostname: cookieHostname } = req.cookies ?? {}
+    const headers = Object.fromEntries( 
+        Object.entries(req.headers ?? {}).filter(([ key, value ]) => !(key in ['referer']))
+    ) as Record<string, any>
+    queryHostname = queryHostname instanceof Array? queryHostname[0] : queryHostname
+    const hostname = queryHostname ?? cookieHostname
+    const pathname = new URL(req.url).pathname
+    const url = hostname? format({ 
+        protocol: 'https',
+        hostname,
+        pathname
+    }) : null
+
+    if (!url) { return new Response(null, { status: 400 }) }
+    const response = await fetch(url, { headers })
+    response.headers.append(
+        "Set-Cookie",
+        `hostname=${hostname}; Path=/`
+    );
+    return response
+}
--- api/index.ts
+++ api/index.ts
@@ -1,28 +1,10 @@
-import { format } from 'url'
-import { ExtendedRequest } from "./types";
+import express from "express";
+import handler from "./handler.js";
 
 
-export default async function handler(req: ExtendedRequest) { console.log(req.url)
-    if (!req.url) { return new Response(null, { status: 400 }) }
-    let { hostname: queryHostname } = req.query ?? {}
-    const { hostname: cookieHostname } = req.cookies ?? {}
-    const headers = Object.fromEntries( 
-        Object.entries(req.headers ?? {}).filter(([ key, value ]) => !(key in ['referer']))
-    ) as Record<string, any>
-    queryHostname = queryHostname instanceof Array? queryHostname[0] : queryHostname
-    const hostname = queryHostname ?? cookieHostname
-    const pathname = new URL(req.url).pathname
-    const url = hostname? format({ 
-        protocol: 'https',
-        hostname,
-        pathname
-    }) : null
+const app = express();
+app.use('/home', express.static('../public/home'));
+app.get("/", handler as any);
 
-    if (!url) { return new Response(null, { status: 400 }) }
-    const response = await fetch(url, { headers })
-    response.headers.append(
-        "Set-Cookie",
-        `hostname=${hostname}; Path=/`
-    );
-    return response
-}
+app.listen(3000, () => console.log("Server ready on port 3000."));
+export default app;
