4432b71268966b877dcc243712cebdc4365a6079
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2025-06-02 15:11:48 -0300

    include referer hostname handling fallback

--- api/handler.ts
+++ api/handler.ts
@@ -3,16 +3,18 @@ import { ExtendedRequest } from "./types";
 
 
 export default async function handler(req: ExtendedRequest) { 
-    let { hostname: queryHostname } = req.query ?? {}
-    const { hostname: cookieHostname } = req.cookies ?? {}
-    const headers = Object.fromEntries( 
+    /* const headers: HeadersInit = Object.fromEntries( 
         Object.entries(req.headers ?? {}).filter(([ key, value ]) => !['referer', 'host'].includes(key))
-    ) as Record<string, any>
+    ) as Record<string, any> */
     const query = Object.fromEntries(
         Object.entries(req.query ?? {}).filter(([ key, value ]) => key!=='hostname')
     )
+    let { hostname: queryHostname } = req.query ?? {}
+    const { hostname: cookieHostname } = req.cookies ?? {}
+    const refererHostname = req.headers.referer? 
+        new URL(req.headers.referer).searchParams.get('hostname') : null
     queryHostname = queryHostname instanceof Array? queryHostname[0] : queryHostname
-    const hostname = queryHostname ?? cookieHostname
+    const hostname = queryHostname ?? cookieHostname ?? refererHostname
     const pathname = req.url?.slice(0, req.url.indexOf('?'))
     const targetUrl = hostname? format({ 
         protocol: 'https',
@@ -23,7 +25,8 @@ export default async function handler(req: ExtendedRequest) {
 
     if (!targetUrl) { return new Response(JSON.stringify({ 
         query: queryHostname,
-        cookie: cookieHostname
+        cookie: cookieHostname,
+        referer: refererHostname
     }), { status: 400 }) }
     const response = await fetch(targetUrl)
     return new Response(await response.text(), { 
