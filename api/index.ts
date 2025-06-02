53b92054e9bd1db1d49459c7079bf8fbfc0b9463
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2025-06-02 13:43:12 -0300

    add handling for / route

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,17 @@
-import express from "express";
+import express, { request, response } from "express";
 import handler from "./handler.js";
 import fs from 'fs'
 
 
+
 const app = express();
+const expressHandler = async(req: typeof request, res: typeof response) => { 
+    const response = await handler(req as any).catch(err => ( 
+        new Response(err?.stack ?? err, { status: 500 })
+    ))
+    res.header('Content-Security-Policy', "default-src 'self'; connect-src 'self'")
+    res.status(response.status).send(await response.text())
+}
 //app.use('/home', express.static('../public/home'));
 /* app.get('/list', (req, res) => { 
     res.send({ 
@@ -11,13 +19,8 @@ const app = express();
         parent: fs.readdirSync('../')
     })
 }); */
-app.get("/*splat", async(req, res) => { 
-    const response = await handler(req as any).catch(err => ( 
-        new Response(err?.stack ?? err, { status: 500 })
-    ))
-    res.header('Content-Security-Policy', "default-src 'self'; connect-src 'self'")
-    res.status(response.status).send(await response.text())
-});
+app.get("/*splat", expressHandler)
+app.get("/", expressHandler)
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
-export default app;
+app.listen(3000, () => console.log("Server ready on port 3000."))
+export default app
