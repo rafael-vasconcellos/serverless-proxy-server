f5a304a441ffd5e0d4faec23a45906dff4562f7b
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2025-06-02 00:35:05 -0300

    fix proxy route handling to use app.all for all requests

--- api/index.ts
+++ api/index.ts
@@ -5,18 +5,18 @@ import fs from 'fs'
 
 const app = express();
 //app.use('/home', express.static('../public/home'));
-app.get("/", async(req, res) => { 
-    const response = await handler(req as any).catch(err => ( 
-        new Response(err, { status: 500 })
-    ))
-    res.status(response.status).send(await response.text())
-});
-app.get('/list', (req, res) => { 
+/* app.get('/list', (req, res) => { 
     res.send({ 
         current: fs.readdirSync('./api'),
         parent: fs.readdirSync('../')
     })
-})
+}); */
+app.get("/*splat", async(req, res) => { 
+    const response = await handler(req as any).catch(err => ( 
+        new Response(err?.stack ?? err, { status: 500 })
+    ))
+    res.status(response.status).send(await response.text())
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 export default app;
